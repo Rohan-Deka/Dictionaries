5583498ee0a444c8335d704979838b874a424bb7
RonDeka <>
2021-05-14 06:31:26 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+programming_dictionary = {
+    "Bug":"An Error in a program that prevents the program from running as expected.",
+    "Function":"Set of defined rules in a program.",
+    
+}
+programming_dictionary["Loop"] = "Repeatinng a particular funtion in a program in order to achieve somethimg."
+for key in programming_dictionary:
+    print(key)
+    print(programming_dictionary[key])
